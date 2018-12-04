e0140c10b513ca075ccce831dcb5d7dab7ad5b73
Ryan McHale <mrryanmchale@outlook.com>
2018-12-04 14:24:21 -0500

    changed my mind
    restructured pin interface

--- pin.hpp
+++ pin.hpp
@@ -1,28 +1,13 @@
 #ifndef _PIN_H_P_P_
 #define _PIN_H_P_P_
 
-constexpr enum class PinMode {
-  dig_out,
-  pwm_out,
-  read_analog,
-  read_pullup,
-  read_pulldown,
-  read
-};
-
-template<PinMode PM>
-class pin
+class pin_base
 {
   const uint8_t pin;
 public:
   pin(const char& pin_num);
   pin(const pin&) = delete; // A disaster waiting to happen
   pin(pin&&);
-  
-  void set(const bool& state);
-  void set(const double& value);
-  
-  auto read();
 };
 
 #endif /* _PIN_H_P_P_ */
