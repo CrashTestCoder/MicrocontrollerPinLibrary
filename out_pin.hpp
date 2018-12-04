e46a75f0c0c7a964ed894aea4147f724db97f137
Ryan McHale <mrryanmchale@outlook.com>
2018-12-04 14:30:39 -0500

    Create out_pin.hpp

---
+++ out_pin.hpp
@@ -0,0 +1,10 @@
+#ifndef _OUTPUT_PIN_H_P_P_
+#define _OUTPUT_PIN_H_P_P_
+
+class out_pin : public pin_base
+{
+  void set(const bool&);
+  void set(const double&);
+};
+
+#endif /* _OUTPUT_PIN_H_P_P_ */
