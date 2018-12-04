9776d081066ed30f23f5e1c3b767dd14163726d5
Ryan McHale <mrryanmchale@outlook.com>
2018-12-04 14:43:06 -0500

    Create pin_msp430x53.hpp

---
+++ pin_msp430x53.hpp
@@ -0,0 +1,13 @@
+#ifndef _MSP430_X53_PIN_LIB_H_P_P_
+#define _MSP430_X53_PIN_LIB_H_P_P_
+
+/*******************************************************/
+/*          Microcontroller Specific Data              */
+/*******************************************************/
+
+
+
+#include "out_pin.hpp"
+#include "input_pin.hpp"
+
+#endif /* _MSP430_X53_PIN_LIB_H_P_P_ */
